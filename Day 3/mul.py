a12588058bb0e1684313965dcd40edfa0269ca33
jcallejo <jcallejo@student.42malaga.com>
2024-12-04 16:50:57 +0100

    Day 3 part 2 and Day 4 done

--- Day 3/mul.py
+++ Day 3/mul.py
@@ -1,21 +1,38 @@
 #Part 1
+from uaclient.cli import security_status_parser
 
 file = open("corrupt_data.txt", 'r')
 lines = file.readlines()
 file.close()
 result = 0
 for line in lines:
-    split = line.split('mul(')[1:]
-    for group in split:
-        pair = group.split(")", maxsplit=1)[0]
-        try:
-            first_number, second_number = pair.split(",", maxsplit = 1)
-        except ValueError:
-            continue
+	split = line.split('mul(')[1:]
+	for group in split:
+		pair = group.split(")", maxsplit=1)[0]
+		try:
+			first_number, second_number = pair.split(",", maxsplit = 1)
+		except ValueError:
+			continue
 
-        if (first_number.isdigit()) and (second_number.isdigit()):
-            result += int(first_number) * int(second_number)
+		if (first_number.isdigit()) and (second_number.isdigit()):
+			result += int(first_number) * int(second_number)
 print(f"Part 1: {result}")
 
 #Part 2
 
+result = 0
+for line in lines:
+	splitto = line.split("do()")
+	for subSplit in splitto:
+		subSplit = subSplit.split("don't()", 1)[0]
+	split = subSplit.split('mul(')[1:]
+	for group in split:
+		pair = group.split(")", maxsplit=1)[0]
+		try:
+			first_number, second_number = pair.split(",", maxsplit=1)
+		except ValueError:
+			continue
+
+		if (first_number.isdigit()) and (second_number.isdigit()):
+			result += int(first_number) * int(second_number)
+print(f"Part 2: {result}")
